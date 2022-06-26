e0f3d5d6af939e3d2475f87a1f9a8632f438dfb3
mortvicious <shindo.shitai@gmail.com>
2022-06-26 14:10:25 +0400

    global

--- server/index.js
+++ server/index.js
@@ -28,22 +28,22 @@ app.use('/api/post', postRouter)
 app.use('/api/user', userRouter)
 app.use(express.static(path.join(__dirname, '../client/build')));
 
-// const start = async () => {
-//     try {
-//         await mongoose.connect(config.get('dbUrl'), {useNewUrlParser:true, useUnifiedTopology:true})
-//         const db = mongoose.connection
-//         db.on('error', console.error.bind(console, `Error MongoDB`))
-//         app.listen(process.env.PORT, () => {
-//             console.log(`Server started on port: ${PORT }`)
-//             console.log(mongoose.connection.readyState)
-//         })
-//     } catch (e) {
+const start = async () => {
+    try {
+        await mongoose.connect(config.get('dbUrl'), {useNewUrlParser:true, useUnifiedTopology:true})
+        const db = mongoose.connection
+        db.on('error', console.error.bind(console, `Error MongoDB`))
+        app.listen(process.env.PORT, () => {
+            console.log(`Server started on port: ${PORT }`)
+            console.log(mongoose.connection.readyState)
+        })
+    } catch (e) {
 
-//     }
-// }
+    }
+}
 
 
-// start()
+start()
 
 
 
