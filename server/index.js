c58ea8b1435e2b88972f611a16f4c0a479f79455
mortvicious <shindo.shitai@gmail.com>
2022-06-26 14:00:24 +0400

    global

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
+#!/usr/bin/env node
+
 const express = require('express')
 const mongoose = require('mongoose')
-const config = require('config')
-// const PORT = config.get('serverPort')
-const PORT = 5000
+// const config = require('config')
+const path = require('path');
 const cookieParser = require('cookie-parser')
 const authRouter = require('./Routes/auth.routes')
 const postRouter = require('./Routes/post.routes')
@@ -27,22 +28,22 @@ app.use('/api/post', postRouter)
 app.use('/api/user', userRouter)
 app.use(express.static(path.join(__dirname, '../client/build')));
 
-const start = async () => {
-    try {
-        await mongoose.connect(config.get('dbUrl'), {useNewUrlParser:true, useUnifiedTopology:true})
-        const db = mongoose.connection
-        db.on('error', console.error.bind(console, `Error MongoDB`))
-        app.listen(PORT, () => {
-            console.log(`Server started on port: ${PORT }`)
-            console.log(mongoose.connection.readyState)
-        })
-    } catch (e) {
+// const start = async () => {
+//     try {
+//         await mongoose.connect(config.get('dbUrl'), {useNewUrlParser:true, useUnifiedTopology:true})
+//         const db = mongoose.connection
+//         db.on('error', console.error.bind(console, `Error MongoDB`))
+//         app.listen(process.env.PORT, () => {
+//             console.log(`Server started on port: ${PORT }`)
+//             console.log(mongoose.connection.readyState)
+//         })
+//     } catch (e) {
 
-    }
-}
+//     }
+// }
 
 
-start()
+// start()
 
 
 
